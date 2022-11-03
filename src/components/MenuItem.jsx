e7e19fea0144264b4ee71d146d5019d7ef737cac
nskMeDVeD <tarasov.a.s.1989@gmail.com>
2022-11-03 23:43:53 +0700

    first commit

--- src/components/MenuItem.jsx
+++ src/components/MenuItem.jsx
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const MenuItem = (props) => {
+    const [count, setCount] = useState(0)
     return (
-        <div className='menu_card'>
+        <div className='menu_card_item'>
             <div className="menu_card_header">
                 <h4>{props.dish.title}</h4> из категории {props.dish.category}
+                <h3>{props.dish.image}</h3>
+                <img src={props.dish.image} alt={props.dish.title}/>
             </div>
-            <div className="menu_card_header">
+            <div className="menu_card_description">
                 {props.dish.ingredients}
             </div>
             <div className="menu_card_footer">
-                Цена и кнопка добавления в корзину
+                <span>{props.dish.price} рублей</span>
+                <div className="menu_card_footer_counter">
+                    <button
+                        onClick={()=>setCount(count-1)}
+                    >-</button>
+                    <input
+                        onChange={event=>setCount(event.target.value)}
+                        value={count}></input>
+                    <button
+                        onClick={()=>setCount(count+1)}
+                    >+</button>
+                </div>
+
+
+
             </div>
             <hr/>
         </div>
